c706778de7ca59497e8230c248230348ff0a01be
FlavioMalvestitiJunior <flavio.malvestiti@outlook.com>
2020-06-22 15:53:37 -0300

    start project

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! '+process.env.PORT);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port process.env.PORT!');
 });
