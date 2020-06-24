fe23ad993e6f56097e68e11c5dbb283044dab692
FlavioMalvestitiJunior <flavio.malvestiti@outlook.com>
2020-06-24 00:39:29 -0300

    up new version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! Avalonian Scheduler');
+  res.send('Hello World! Avalonian Scheduler Now On heroku');
 });
 
 app.listen(process.env.PORT, function () {
